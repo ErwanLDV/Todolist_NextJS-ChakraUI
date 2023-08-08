68046c6277bd1073160d53be9c3d0b49b63e66c9
erwan <e.lable@yahoo.fr>
2023-08-09 01:41:40 +0200

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import { Flex, Heading, Text, Input, Button } from "@chakra-ui/react"
+
+const Header = () => {
+  return (
+    <>
+    <Flex p={"2rem"} direction={"column"} alignItems={"center"}>
+      <Heading as={"h1"} size={"4xl"} noOfLines={1} className="tasklist-title">TaskList.io</Heading>
+      <Text mt={'1rem'} className="tasklist-slogan">
+        TaskList est un outil open-source qui vous simplifie votre quotidien en toute éfficacité.
+      </Text>
+    </Flex>
+    </>
+  )
+}
+
+export default Header
